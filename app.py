74ce3e2cf1a703c8aaff9927216aed9b6c85eca2
yousefshawky <youssef.shawky@ucalgary.ca>
2024-06-16 14:33:03 +0400

    debugging app.py

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def start_conversion():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001)
+    socketio.run(app)
